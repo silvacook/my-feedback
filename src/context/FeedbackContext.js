2d6626870d3424841fb01a67f5f26e6316457eda
silvacook <robertsilva1994@yahoo.com>
2023-11-07 13:09:08 -0800

    Edit

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -61,8 +61,7 @@ export const FeedbackProvider = ({ children }) => {
         const data = await response.json()
 
         setFeedback(
-            feedback.map((item) => item.id === id ? {...item, ...
-            data } : item ))
+            feedback.map((item) => item.id === id ? {...item, ...data } : item))
     }
 
     // Set item to be updated
